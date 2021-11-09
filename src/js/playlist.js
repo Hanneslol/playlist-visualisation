aa3c87b1bb50d5c0013baf6a3cc37a308e3b717a
Hanneslol <hannes@hanneswallin.com>
2021-11-09 17:14:50 +0100

    showing cover and play/pause sample
    showing cover and play/pause sample

--- src/js/playlist.js
+++ src/js/playlist.js
@@ -1,3 +1,5 @@
+const { doc } = require("prettier");
+
 /*
 
   Follow the instructions for "Calling the Spotify playlist API" and
@@ -24,6 +26,68 @@ function getPlaylist(playlist_id) {
   }
 
   return fetch(url, { headers }).then((response) => response.json());
-
 }
-getPlaylist('4fnGbuyTNDy1EekNV3hhfH').then((response) => console.log(response));
+
+function renderPlaylist(playlist_id) {
+  /*
+  <div class="playlist-item">
+    <img class="playlist-item-img" src="IMG_URL" />
+    <div class="playlist-item-title">SONG_TITLE</div>
+  </div>
+ 
+  */
+
+  const container = document.getElementById('tracks');
+  const audioPlayer = document.getElementById('player')
+  getPlaylist(playlist_id).then((playlist) => {
+    const tracks = playlist.tracks.items;
+    for (let i = 0; i < tracks.length; i++) {
+      const track = tracks[i].track;
+
+      const playlistItem = document.createElement('div');
+      playlistItem.classList.add('playlist-item')
+
+      const playlistItemImg = document.createElement('img');
+      playlistItemImg.classList.add('playlist-item-img')
+      playlistItemImg.setAttribute('src', track.album.images[0].url);
+
+      const playlistItemTitle = document.createElement('div')
+      playlistItemTitle.classList.add('playlist-item-title');
+      playlistItemTitle.innerHTML = track.name;
+
+      playlistItem.addEventListener('click', () => {
+        if (currentlyActive === track.id) {
+          audioPlayer.pause();
+          currentlyActive = null;
+          playlistItem.classList.remove('active')
+        } else {
+          if (currentlyActive) {
+            document.querySelector('.active').classList.remove('active');
+          }
+          currentlyActive = track.id;
+          playlistItem.classList.add('active');
+
+
+          //Play preview if available
+
+          if (track.preview_url) {
+            audioPlayer.setAttribute('src', track.preview_url);
+            audioPlayer.play();
+          }
+          else {
+            audioPlayer.pause()
+          }
+        }
+      })
+
+      playlistItem.appendChild(playlistItemImg)
+      playlistItem.appendChild(playlistItemTitle)
+      container.appendChild(playlistItem)
+    }
+
+
+  })
+}
+
+let currentlyActive;
+renderPlaylist('4fnGbuyTNDy1EekNV3hhfH');
