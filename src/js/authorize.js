1e997b9eb450e04755325eb530a82a375185a4d2
Hanneslol <hannes@hanneswallin.com>
2021-11-09 11:54:04 +0100

    Authentization
    auth

--- src/js/authorize.js
+++ src/js/authorize.js
@@ -3,3 +3,17 @@
   Follow the instructions for "Calling the Spotify playlist API" in the README 
 
 */
+console.log(process.env.CLIENT_ID);
+const loginButton = document.getElementById('login-button')
+loginButton.addEventListener('click', () => {
+  const client_id = process.env.CLIENT_ID;
+  const redirect_uri = 'http://localhost:8080/playlist.html';
+
+  let url = 'https://accounts.spotify.com/authorize';
+  url += '?response_type=token';
+  url += '&client_id=' + encodeURIComponent(client_id);
+  url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+
+  window.location.href = url;
+
+});
--- src/js/playlist.js
+++ src/js/playlist.js
@@ -4,3 +4,26 @@
   "Rendering a playlist visualisation" in the README 
 
 */
+function getAccessToken() {
+  const hash = window.location.hash;
+  const hashwithouthash = hash.substring(1);
+  const params = hashwithouthash.split('&');
+  const keyValues = params.map((param) => param.split('='));
+  const accessToken = keyValues[0][1];
+  return accessToken;
+
+  console.log(keyValues);
+}
+
+function getPlaylist(playlist_id) {
+  const url = `https://api.spotify.com/v1/playlists/${playlist_id}`;
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'applitcations.json',
+    Authorization: `Bearer ${getAccessToken()}`,
+  }
+
+  return fetch(url, { headers }).then((response) => response.json());
+
+}
+getPlaylist('4fnGbuyTNDy1EekNV3hhfH').then((response) => console.log(response));
